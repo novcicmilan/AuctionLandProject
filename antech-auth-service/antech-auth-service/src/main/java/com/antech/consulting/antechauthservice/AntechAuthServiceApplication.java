44c5def883bb6797808085535a5722d34cf5286b
Novcic Milan <novcic.milan17@uns.ac.rs>
2022-02-17 11:31:39 +0100

    Initial parent-child relationship
    Relationship is one sided, there are only modules in parent pom.xml but not parent in child pom.xml files

---
+++ antech-auth-service/antech-auth-service/src/main/java/com/antech/consulting/antechauthservice/AntechAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.antech.consulting.antechauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntechAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntechAuthServiceApplication.class, args);
+	}
+
+}
